bac94628d034ba2b30baa7431b6bde695b0a93fc
arnaudandriambao7-dev <arnaudandriambao7@gmail.com>
2026-09-14 19:37:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const HERO_IMG_URL = 'https://image.tmdb.org/t/p/original';
 
 // Lecteurs vidéo optimisés pour le contenu multilingue / VF
 const PLAYERS = {
- movie: [
-    { name: 'Lecteur 1 (VF / STFR)', url: 'https://vidsrc.me/embed/movie?tmdb=' },
-    { name: 'Lecteur 2 (AutoEmbed + STFR)', url: 'https://player.autoembed.cc/embed/movie/' },
-    { name: 'Lecteur 3 (VidSrc SBS + STFR)', url: 'https://vidsrc.sbs/embed/movie/' }
+movie: [
+    { name: 'Lecteur 1 (Multi / VF)', url: 'https://vidsrc.vip/embed/movie/' },
+    { name: 'Lecteur 2 (VidSrc me)', url: 'https://vidsrc.me/embed/movie?tmdb=' },
+    { name: 'Lecteur 3 (SuperEmbed)', url: 'https://multiembed.mov/?video_id=' }
   ],
   tv: [
-    { name: 'Lecteur 1 (VF / STFR)', url: (id, s, e) => `https://vidsrc.me/embed/tv?tmdb=${id}&season=${s}&episode=${e}&sub=fr` },
-    { name: 'Lecteur 2 (AutoEmbed + STFR)', url: (id, s, e) => `https://player.autoembed.cc/embed/tv/${id}/${s}/${e}` },
-    { name: 'Lecteur 3 (VidSrc SBS + STFR)', url: (id, s, e) => `https://vidsrc.sbs/embed/tv/${id}/${s}/${e}?sub=fr` }
+    { name: 'Lecteur 1 (Multi / VF)', url: (id, s, e) => `https://vidsrc.vip/embed/tv/${id}/${s}/${e}` },
+    { name: 'Lecteur 2 (VidSrc me)', url: (id, s, e) => `https://vidsrc.me/embed/tv?tmdb=${id}&season=${s}&episode=${e}` },
+    { name: 'Lecteur 3 (SuperEmbed)', url: (id, s, e) => `https://multiembed.mov/?video_id=${id}&tmdb=1&s=${s}&e=${e}` }
   ]
 };
 
@@ -282,11 +282,8 @@ function loadStream() {
  if (currentMediaType === 'movie') {
     const server = PLAYERS.movie[selectedServerIndex];
     if (videoPlayer) {
-      if (server.url.includes('vidsrc.me')) {
-        // Force la langue française et sous-titres FR
-        videoPlayer.src = `${server.url}${activeMediaId}&sub=fr&lang=fr`;
-      } else if (server.url.includes('vidsrc.sbs')) {
-        videoPlayer.src = `${server.url}${activeMediaId}?sub=fr`;
+      if (server.url.includes('multiembed')) {
+        videoPlayer.src = `${server.url}${activeMediaId}&tmdb=1`;
       } else {
         videoPlayer.src = `${server.url}${activeMediaId}`;
       }
